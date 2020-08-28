c1d264af18f6cbed16a83a26876c5dc23c48b8c6
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-28 19:10:58 -0300

    Update main screen

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,111 +1,161 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Keyboard, ActivityIndicator } from 'react-native';
+import { Keyboard, ActivityIndicator, Alert } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import * as Yup from 'yup';
+import { useNavigation } from '@react-navigation/native';
 
-import api from '~/services/api';
+import api from '~/services/github';
 import {
   Container,
+  Header,
+  HeaderImage,
   Form,
+  Label,
+  Search,
   Input,
-  SubmitButton,
+  Button,
+  Error,
   List,
   User,
   Avatar,
+  About,
   Name,
   Bio,
-  ProfileButton,
-  ProfileButtonText,
+  Actions,
+  Action,
 } from './styles';
 
-export default function Main({ navigation }) {
+export default () => {
   const [newUser, setNewUser] = useState('');
-  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const starred_users = await AsyncStorage.getItem('users');
-
-      if (starred_users) {
-        setUsers(starred_users);
-      }
-    })();
-  }, []);
+  const navigation = useNavigation();
 
   const handleAddUser = useCallback(async () => {
-    (async () => {
-      if (newUser.length > 0) {
-        setLoading(true);
+    try {
+      setLoading(true);
+      setError('');
+
+      const foundUser = users.find(
+        user => user.login.toLowerCase() === newUser.toLowerCase(),
+      );
+
+      if (foundUser) {
+        setError('Este usuário já foi adicionado!');
+      } else {
+        const schema = Yup.string('Deve ser um usuário válido')
+          .min(3, 'Deve conter pelo menos 3 caracteres')
+          .required('Digite um usuário válido');
+
+        await schema.validate(newUser, { abortEarly: false });
 
         const { data } = await api.get(`/users/${newUser}`);
         const user = {
-          name: data.name,
-          login: data.login,
-          bio: data.bio,
-          avatar: data.avatar_url,
+          name: data.name.trim(),
+          login: data.login.trim(),
+          bio: data.bio.trim(),
+          avatar: data.avatar_url.trim(),
         };
 
-        setUsers([...users, user]);
+        setUsers([user, ...users]);
         setNewUser('');
-        setLoading(false);
-        await AsyncStorage.setItem('users', JSON.stringify([...users, user]));
-      }
 
+        await AsyncStorage.setItem('users', JSON.stringify([user, ...users]));
+      }
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        setError(err.inner.pop().message);
+      } else {
+        Alert.alert('Ops! Alguma coisa deu errado, tente novamente!');
+      }
+    } finally {
+      setLoading(false);
       Keyboard.dismiss();
-    })();
-  }, [newUser]);
+    }
+  }, [newUser, users]);
+
+  const handleRemoveUser = useCallback(
+    async login => {
+      const updatedUsers = users.filter(user => user.login !== login);
+
+      setUsers(updatedUsers);
+      await AsyncStorage.setItem('users', JSON.stringify(updatedUsers));
+    },
+    [users],
+  );
 
   const handleNavigate = useCallback(user => {
     navigation.navigate('User', { user });
   }, []);
 
+  useEffect(() => {
+    AsyncStorage.getItem('users').then(starred_users => {
+      if (starred_users) {
+        setUsers(JSON.parse(starred_users));
+      }
+    });
+  }, []);
+
   return (
     <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-          value={newUser}
-          onChangeText={text => setNewUser(text)}
-          returnKeyType="send"
-          onSubmitEditing={handleAddUser}
-        />
-        <SubmitButton testID="submit" loading={loading} onPress={handleAddUser}>
-          {loading ? (
-            <ActivityIndicator color="#FFF" />
-          ) : (
-            <Icon name="add" size={20} color="#FFF" />
-          )}
-        </SubmitButton>
-      </Form>
+      <Header>
+        <HeaderImage />
+        <Form>
+          <Label>Explore usuários no GitHub:</Label>
+          <Search>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Digite um nome de usuário"
+              value={newUser}
+              onChangeText={text => setNewUser(text)}
+              returnKeyType="send"
+              onSubmitEditing={handleAddUser}
+            />
+            <Button onPress={handleAddUser} testID="add_user">
+              {loading ? (
+                <ActivityIndicator color="#FFF" />
+              ) : (
+                <Icon name="search" size={27} color="#FFF" />
+              )}
+            </Button>
+          </Search>
+
+          {error.length > 0 && <Error>{error}</Error>}
+        </Form>
+      </Header>
 
       <List
         data={users}
         keyExtractor={user => user.login}
         renderItem={({ item: user }) => (
-          <User testID={`user_${user.login}`}>
+          <User
+            testID={`user_${user.login}`}
+            onPress={() => handleNavigate(user)}
+          >
             <Avatar source={{ uri: user.avatar }} />
-            <Name>{user.name}</Name>
-            <Bio>{user.bio}</Bio>
 
-            <ProfileButton
-              testID={`user_profile_${user.login}`}
-              onPress={() => handleNavigate(user)}
-            >
-              <ProfileButtonText>Ver perfil</ProfileButtonText>
-            </ProfileButton>
+            <About>
+              <Name>{user.name}</Name>
+              <Bio>{user.bio}</Bio>
+            </About>
+
+            <Actions>
+              <Action
+                testID={`user_remove_${user.login}`}
+                onPress={() => {
+                  handleRemoveUser(user.login);
+                }}
+              >
+                <Icon name="delete" color="#fff" size={15} />
+              </Action>
+            </Actions>
           </User>
         )}
       />
     </Container>
   );
-}
-
-Main.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func,
-  }).isRequired,
 };
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,62 +1,103 @@
 import styled from 'styled-components/native';
-import { RectButton } from 'react-native-gesture-handler';
+
+import Logo from '~/assets/logo.png';
 
 export const Container = styled.View`
+  background-color: #f0f0f5;
   flex: 1;
-  padding: 30px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  background-color: #121214;
+  padding: 10px 20px 10px;
+`;
+
+export const HeaderImage = styled.Image.attrs({
+  source: Logo,
+  resizeMode: 'contain',
+})`
+  width: 165px;
 `;
 
 export const Form = styled.View`
-  border-color: #eee;
-  border-bottom-width: 1px;
+  width: 100%;
+  margin-top: 20px;
+`;
+
+export const Label = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-left: 4px;
+`;
+
+export const Search = styled.View`
+  align-items: center;
   flex-direction: row;
-  padding-bottom: 20px;
+  margin-top: 5px;
+  position: relative;
+`;
+
+export const Button = styled.TouchableOpacity`
+  padding: 5px;
+  position: absolute;
+  right: 7px;
 `;
 
 export const Input = styled.TextInput.attrs({
-  placeholderTextColor: '#999',
+  placeholderTextColor: '#73738044',
 })`
-  background-color: #eee;
-  border: 1px solid #eee;
-  border-radius: 4px;
+  background-color: #232325;
+  border-radius: 6px;
+  color: #737380;
   flex: 1;
-  height: 40px;
-  padding: 0px 15px;
+  height: 50px;
+  padding: 0px 50px 0px 15px;
 `;
 
-export const SubmitButton = styled(RectButton)`
-  align-items: center;
-  background-color: #7159c1;
-  border-radius: 4px;
-  justify-content: center;
-  margin-left: 10px;
-  padding: 0px 12px;
-  opacity: ${props => (props.loading ? 0.7 : 1)};
+export const Error = styled.Text`
+  color: #e23337;
+  margin-left: 5px;
+  margin-top: 7px;
+  text-align: center;
 `;
 
 export const List = styled.FlatList.attrs({
   showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: 10,
+  },
 })`
-  margin-top: 20px;
+  padding: 10px 0px 50px;
 `;
 
-export const User = styled.View`
-  align-items: center;
-  margin: 0px 20px 30px;
+export const User = styled.TouchableOpacity`
+  background-color: #ffffff;
+  border-radius: 10px;
+  flex-direction: row;
+  margin: 0px 20px 10px;
+  padding: 8px;
 `;
 
 export const Avatar = styled.Image`
   background-color: #eee;
-  border-radius: 32px;
-  height: 64px;
-  width: 64px;
+  border-radius: 10px;
+  height: 58px;
+  width: 58px;
+`;
+
+export const About = styled.View`
+  flex: 1;
+  align-items: flex-start;
+  margin-left: 10px;
+  justify-content: center;
 `;
 
 export const Name = styled.Text`
   color: #333;
   font-size: 14px;
   font-weight: bold;
-  margin-top: 4px;
   text-align: center;
 `;
 
@@ -65,24 +106,17 @@ export const Bio = styled.Text.attrs({
 })`
   color: #999;
   font-size: 13px;
-  line-height: 18px;
-  margin-top: 5px;
-  text-align: center;
+  line-height: 17px;
 `;
 
-export const ProfileButton = styled(RectButton)`
-  align-items: center;
-  align-self: stretch;
-  background-color: #7159c1;
-  border-radius: 4px;
-  height: 36px;
-  margin-top: 10px;
+export const Actions = styled.View`
   justify-content: center;
+  width: 30px;
 `;
 
-export const ProfileButtonText = styled.Text`
-  color: #fff;
-  font-size: 14px;
-  font-weight: bold;
-  text-transform: uppercase;
+export const Action = styled.TouchableOpacity`
+  border-radius: 20px;
+  background-color: #dfdfdf;
+  padding: 5px;
+  width: 25px;
 `;
