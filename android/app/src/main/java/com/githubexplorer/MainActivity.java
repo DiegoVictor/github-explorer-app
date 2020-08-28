66f40fbd580e7de58a0761dee4133d0ba9abe746
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-27 22:38:39 -0300

    Update folder name

--- android/app/src/main/java/com/githubexplorer/MainActivity.java
+++ android/app/src/main/java/com/githubexplorer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.githubusersstarsviewer;
+package com.githubexplorer;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "GithubUsersStarsViewer";
+        return "GitHubExplorer";
     }
 
     @Override
