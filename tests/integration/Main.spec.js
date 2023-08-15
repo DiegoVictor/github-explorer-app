0d00bf98972152aad95ebe583b02d3c8c4065fcc
Diego Victor <diegovictorgonzaga@gmail.com>
2023-08-14 22:32:36 -0300

    Fix lint

--- tests/integration/Main.spec.js
+++ tests/integration/Main.spec.js
@@ -23,8 +23,9 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
-jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
-
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
 
 describe('Main page', () => {
   const apiMock = new MockAdapter(api);
